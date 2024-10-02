a53a817af070db4f5b29017d2611d6569c96f0d7
Richard ZC <badboys_jordan@hotmail.com>
2024-10-01 18:26:08 -0700

    Configuración del servidor

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const instrumentRoutes = require('./routes/instrumentRoutes');
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/instruments', instrumentRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
